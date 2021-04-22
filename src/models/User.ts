0892b2c6a80c3b8339b3f5830cba717d998415af
Clizio <clizioguedes@gmail.com>
2021-04-22 03:57:16 -0300

    Configuracao do dotenv

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
-@Entity("users")
+@Entity()
 class User {
   @PrimaryColumn()
   readonly id: string;
